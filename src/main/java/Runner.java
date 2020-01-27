63499b7a85e689d42bb32b9290e28a2cb169d151
Grant Houston <grant.houston44@hotmail.com>
2020-01-27 14:57:44 +0000

    Initial commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,7 @@
+public class Runner {
+    public static void main(String[] args){
+    Planet mars = new Planet("Mars", 908973);
+    System.out.println(String.format("%s is %s", mars.getName(), mars.getSize()));
+    mars.explode();
+    }
+}
---
+++ src/test/java/PlanetTest.java
@@ -0,0 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals
+
+public class PlanetTest {
+    private Planet mars;
+
+    @Before
+    public void before(){
+        mars = new Planet("mars", 908973);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("mars", mars.getName());
+    }
+
+    @Test
+    public void hasSize(){
+        assertEquals(908973, mars.getSize());
+    }
+
+    @Test
+    public String explodes(){
+        assertEquals("Boom! mars has exploded", mars.explode());
+    }
+}
